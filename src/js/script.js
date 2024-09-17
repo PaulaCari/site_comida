e5accc35314392dc91c122421d5621c1266f93aa
Paula Cari <paulola0223@gmail.com>
2024-09-17 16:43:33 -0300

    adicione img favicon, delete os .css do html

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,4 @@
+// 3Barrar
 $(document).ready(function(){
     $('#mobile_btn').on('click', function(){
         $('#mobile_menu').toggleClass('active')
@@ -6,3 +7,5 @@ $(document).ready(function(){
         $('#mobile_btn').find('i').toggleClass('fa-x')
     });
 });
+
+// animações
