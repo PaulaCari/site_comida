c27a465dfad216cc67f25cdee0fc1a45f5f561dc
Paula Cari <paulola0223@gmail.com>
2024-09-18 09:37:50 -0300

    adicione js, animação quando rolar a pag o header aparece a sombra

--- src/js/script.js
+++ src/js/script.js
@@ -6,6 +6,32 @@ $(document).ready(function(){
         //troca as 3linhas pelo x
         $('#mobile_btn').find('i').toggleClass('fa-x')
     });
+
+
+    // animação quando rolar a pag
+    const sections = $('section');
+    const navItems = $('.nav-item');
+
+    $(window).on ('scroll', function(){
+        //adicionar a sombra
+        const header = $('header');
+        const scrollPosition = $(window).scrollTop() - header.outerHeight();
+        //console.log(scrollPosition)
+
+       //box-shadow 
+       if (scrollPosition <= 0) {
+        header.css('box-shadow', 'none');
+     }
+     else{
+        header.css('box-shadow', '8px 1px 8px rgba(0,0,0,0.2)');
+     }
+
+    });
 });
 
-// animações
+
+
+
+
+
+//animação de aparecer os elementos textos
